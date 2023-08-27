c23706ce8acbff57891ae0821a4ca62adbfa61f1
chi.zhang <624593758@qq.com>
2023-08-27 23:00:43 +0800

    feat: 增加hello_algo 项目

---
+++ hello_algo/array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5] = {0};
+    int nums[5] = {1, 2, 3, 4, 5};
+    printf("sizeof(arr) = %zu\n", sizeof(arr));
+    printf("sizeof(nums) = %zu\n", sizeof(nums));
+}
---
+++ hello_algo/linked_list.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello linked_list\n");
+    return 0;
+};
